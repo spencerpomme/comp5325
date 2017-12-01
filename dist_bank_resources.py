cf09377a5e6778a74bc83dd523259c9ef038fbfc
スペンサー <spencerpomme@live.com>
2017-12-01 22:50:44 +0800

    Code that utilizes data from mock db

---
+++ dist_bank_resources.py
@@ -0,0 +1,26 @@
+# dist_bank_resources.py
+"""
+The balance data json utiliztion code.
+"""
+
+import json
+
+import dist_bank_pb2
+
+
+def read_dist_bank_database():
+    """Reads the dist bank database.
+
+    Returns:
+      The full contents of the dist bank database as a sequence of
+        dist_bank_pb2.BalanceRecord.
+    """
+    record_list = []
+    with open("dist_bank_db.json") as dist_bank_db_file:
+        for item in json.load(dist_bank_db_file):
+            record = dist_bank_pb2.BalanceRecord(
+                uid=item["uid"],
+                index=item["index"],
+                balance=item["balance"])
+            record_list.append(record)
+    return record_list
