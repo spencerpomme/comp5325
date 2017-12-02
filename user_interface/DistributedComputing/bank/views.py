40f3930337445d74748254927a85aea1c2fb3869
shishengjia <shishengjia1@live.com>
2017-12-02 17:36:14 +0800

    已知bug修复 精简代码

--- user_interface/DistributedComputing/bank/views.py
+++ user_interface/DistributedComputing/bank/views.py
@@ -1,12 +1,12 @@
+#encoding: utf-8
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import HttpResponse
-# Create your views here.
+import json
 
 
 class SearchAccountView(View):
     def get(self, request):
-
 
         id = 11
         balance = 500
@@ -14,31 +14,46 @@ class SearchAccountView(View):
 
     def post(self, request):
 
-        if request.POST.get('type', '') == 'search':
-            id = request.POST.get("id", 0)
+        type_ = request.POST.get('type', '')
 
+        #  搜索用户
+        if type_ == 'search':
+            id = request.POST.get("id", 0)
             balance = 100
 
-            # do search by user id
-            return HttpResponse('{"status": "success","balance": balance}',
+            # invoke the function here
+
+            dicts = {"status": "success", "balance": balance, "type": type_, 'id': id}
+
+            return HttpResponse(json.dumps(dicts),
                                 content_type="application/json")
 
-        if request.POST.get('type', '') == 'save':
+        # 存钱
+        if type_ == 'save':
             id = request.POST.get("id", 0)
             amount = request.POST.get('amount', 0)
 
             balance = 200
 
-            return HttpResponse('{"status": "success","balance": balance}',
+            # invoke the function here, and use balance to receive the result
+
+            dicts = {"status": "success", "balance": balance, "type": type_}
+
+            return HttpResponse(json.dumps(dicts),
                                 content_type="application/json")
 
-        if request.POST.get('type', '') == 'withdraw':
+        # 提现
+        if type_ == 'withdraw':
             id = request.POST.get("id", 0)
             amount = request.POST.get('amount', 0)
 
             balance = 300
 
-            return HttpResponse('{"status": "success","balance": balance}',
+            # invoke the function here, and use balance to receive the result
+
+            dicts = {"status": "success", "balance": balance, "type": type_}
+
+            return HttpResponse(json.dumps(dicts),
                                 content_type="application/json")
 
         return HttpResponse('{"status": "fail"}',
