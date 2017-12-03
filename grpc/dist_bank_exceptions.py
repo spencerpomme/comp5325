dcdc0b50116513e6b135a87196dbaaa54a2bbc0e
スペンサー <spencerpomme@live.com>
2017-12-03 12:57:39 +0800

    Add an exception class
    DatabaseOptFailure

--- grpc/dist_bank_exceptions.py
+++ grpc/dist_bank_exceptions.py
@@ -1,8 +1,19 @@
 # dist_bank_exceptions.py
+"""
+Global definitons of project errors and exceptions.
+"""
 
 class AccountNotExistError(Exception):
     """
     Account not exist error.
     """
-    pass
+    def __repr__(self):
+        return "Account not exist error."
+
+class DatabaseOptFailure(Exception):
+    """
+    Methods in dist_bank_resources failed to operate database properly.
+    """
+    def __repr__(self):
+        return "Methods in dist_bank_resources failed to operate database properly."
 
